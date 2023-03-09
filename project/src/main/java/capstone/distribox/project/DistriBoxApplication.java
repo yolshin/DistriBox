901e0a70b0a84c09858e2198c65bdaa6a7800847
Elie Benzaquen <eliebenzaquen@gmail.com>
2023-03-09 17:59:34 -0500

    spring project for distribox

---
+++ project/src/main/java/capstone/distribox/project/DistriBoxApplication.java
@@ -0,0 +1,13 @@
+package capstone.distribox.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistriBoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistriBoxApplication.class, args);
+	}
+
+}
---
+++ project/src/test/java/capstone/distribox/project/DistriBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package capstone.distribox.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistriBoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
