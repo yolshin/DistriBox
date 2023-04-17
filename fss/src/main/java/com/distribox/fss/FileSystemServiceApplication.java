4e61a0701c2f3407332655e8a463ee516dca7873
yolshin <olshin@mail.yu.edu>
2023-04-17 18:41:19 -0400

    init commit

---
+++ fss/src/main/java/com/distribox/fss/FileSystemServiceApplication.java
@@ -0,0 +1,13 @@
+package com.distribox.fss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileSystemServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileSystemServiceApplication.class, args);
+	}
+
+}
---
+++ fss/src/test/java/com/distribox/fss/FileSystemServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.distribox.fss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileSystemServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
