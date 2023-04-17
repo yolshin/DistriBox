3472f58dca5a626f72cb08b77c1944ddb2258881
yolshin <olshin@mail.yu.edu>
2023-04-17 19:46:11 -0400

    init

---
+++ fss/src/main/java/com/distribox/fss/controller/FileSystemServiceController.java
@@ -0,0 +1,14 @@
+package com.distribox.fss.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World!";
+    }
+
+}
