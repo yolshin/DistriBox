c0b0a46b906321b5c38020261fe74f9d7df513bc
Michael Berk <michaelberkdev@gmail.com>
2023-04-18 16:29:44 -0400

    Add fds

---
+++ fds/src/main/java/com/distribox/fds/FileDistributionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.distribox.fds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileDistributionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileDistributionServiceApplication.class, args);
+	}
+
+}
---
+++ fds/src/test/java/com/distribox/fds/FileDistributionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.distribox.fds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileDistributionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
