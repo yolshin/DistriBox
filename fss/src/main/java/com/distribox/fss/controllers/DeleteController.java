bbfd7fdcfd4402df32fadd82813ced7ec1b2cfcc
yolshin <olshin@mail.yu.edu>
2023-04-24 20:21:33 -0400

    started setting up controllers and services

---
+++ fss/src/main/java/com/distribox/fss/controllers/DeleteController.java
@@ -0,0 +1,23 @@
+package com.distribox.fss.controllers;
+
+import com.distribox.fss.services.DeleteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DeleteController {
+
+    @Autowired
+    DeleteService deleteService;
+
+    @DeleteMapping("/delete")
+    public String delete(@RequestBody String file) {
+        // Delete file.
+        deleteService.deleteFile(file);
+        // Send ACK to FDS (?).
+        return "File deleted";
+    }
+
+}
--- fss/src/main/java/com/distribox/fss/services/DeleteService.java
+++ fss/src/main/java/com/distribox/fss/services/DeleteService.java
@@ -4,4 +4,10 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class DeleteService {
+
+    public String deleteFile(String file) {
+        // Delete file.
+        return "File deleted";
+    }
+
 }
