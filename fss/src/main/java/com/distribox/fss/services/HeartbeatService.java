0f43bc1fedd0470edeaf3c8c6ccbcf23bc7060ee
yolshin <olshin@mail.yu.edu>
2023-05-16 21:02:30 -0400

    started setting up controllers and services

--- fss/src/main/java/com/distribox/fss/services/HeartbeatService.java
+++ fss/src/main/java/com/distribox/fss/services/HeartbeatService.java
@@ -1,14 +1,24 @@
 package com.distribox.fss.services;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 public class HeartbeatService {
 
     public void sendHeartbeat() {
         //TODO: make sure that it gets an acknowledgement from the leader
-        //and to implement retry logic if it doesn't
+        // and to implement retry logic if it doesn't
         // Send heartbeat.
+        String ack = "ACK"; // Replace with your ack message
+        WebClient.create().post()
+                .uri("http://file-distribution-service/ack") // Replace with the appropriate URI
+                .contentType(MediaType.APPLICATION_JSON) // Set the content type of the request
+                .bodyValue(ack) // Set the ack data in the request body
+                .retrieve()
+                .bodyToMono(Void.class) // Specify the response type (if needed)
+                .block();
     }
 
 }
